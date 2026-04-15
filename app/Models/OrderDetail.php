2193226fe9af1a664e9a5b45c48fd538ea9f05ec
PhanHuy20 <hp78519@gmail.com>
2026-04-15 21:43:42 +0700

    Hoàn thành giao diện Master Layout

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
-    //
+    protected $fillable = [
+        'order_id', 'motorcycle_id', 'quantity', 'unit_price', 'subtotal'
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function motorcycle()
+    {
+        return $this->belongsTo(Motorcycle::class);
+    }
 }
